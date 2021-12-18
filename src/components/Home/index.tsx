d4fe96dc3dfdcdd154995fb9090dc8521e556f8b
hieugiabao <hieugiabao12@gmail.com>
2021-12-18 21:03:29 +0700

    setup homepage

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,130 +1,34 @@
+import { NetworkStatus } from "@apollo/client";
 import { Link } from "react-router-dom";
+import { useVideosQuery } from "../../generated/graphql";
 import SlickNav from "../SlickNav";
 import styles from "./Home.module.scss";
 
+const limit = 2;
+
 const Home = () => {
-  const data = [
-    {
-      id: "17ZXlVzLb0toTe3dtO8q80DmVlnQz9R99",
-      title: "Tokyo Ghoul",
-      description: "this is description",
-      userId: "1",
-      commentable: true,
-      thumbNailUrl: "https://images8.alphacoders.com/546/thumbbig-546902.webp",
-      size: 1000,
-      createdAt: "09/12/2021",
-      updatedAt: "09/12/2021",
+  const { data, loading, fetchMore, networkStatus } = useVideosQuery({
+    variables: {
+      limit
     },
-    {
-      id: "1r27Pc7Y4p8VALLxl3MUZSvMIAIoK2CGe",
-      title: "Arcane",
-      description: "this is description",
-      userId: "1",
-      commentable: true,
-      thumbNailUrl: "https://images2.alphacoders.com/119/thumbbig-1192178.webp",
-      size: 1000,
-      createdAt: "09/12/2021",
-      updatedAt: "09/12/2021",
-    },
-    {
-      id: "1VOhQq4iMun2ojv9CindmKnd4s4CgAwDd",
-      title: "Sword Art Online",
-      description: "this is description",
-      userId: "1",
-      commentable: true,
-      thumbNailUrl: "https://images8.alphacoders.com/533/thumbbig-533007.webp",
-      size: 1000,
-      createdAt: "09/12/2021",
-      updatedAt: "09/12/2021",
-    },
-    {
-      id: "1HSLvuGOSUfDQWC7ZwZlFobnfzlgGfwld",
-      title: "Naruto",
-      description: "this is description",
-      userId: "1",
-      commentable: true,
-      thumbNailUrl: "https://images3.alphacoders.com/135/thumbbig-135625.webp",
-      size: 1000,
-      createdAt: "09/12/2021",
-      updatedAt: "09/12/2021",
-    },
-    {
-      id: "1c2dBx0KCmX3aAbqRFYaJLxiwIPeOoZ73",
-      title: "Dragon Ball",
-      description: "this is description",
-      userId: "1",
-      commentable: true,
-      thumbNailUrl: "https://images.alphacoders.com/605/thumbbig-605799.webp",
-      size: 1000,
-      createdAt: "09/12/2021",
-      updatedAt: "09/12/2021",
-    },
-    {
-      id: "12STlO4qx1tccXTTvRmdGG8cU57EJEdbK",
-      title: "One Piece Stamp",
-      description: "this is description",
-      userId: "1",
-      commentable: true,
-      thumbNailUrl: "https://images6.alphacoders.com/606/thumbbig-606263.webp",
-      size: 1000,
-      createdAt: "09/12/2021",
-      updatedAt: "09/12/2021",
-    },
-    {
-      id: "1GoZqWSYQXk2YYQz6T4KT27QCiPrSfK2V",
-      title: "Bleach EX",
-      description: "this is description",
-      userId: "1",
-      commentable: true,
-      thumbNailUrl: "https://images3.alphacoders.com/167/thumbbig-16729.webp",
-      size: 1000,
-      createdAt: "09/12/2021",
-      updatedAt: "09/12/2021",
-    },
-    {
-      id: "1XN64i3mSXalozFZ6lFBiDJSlgYHdZztG",
-      title: "Pokemon",
-      description: "this is description",
-      userId: "1",
-      commentable: true,
-      thumbNailUrl: "https://images5.alphacoders.com/613/thumbbig-613925.webp",
-      size: 1000,
-      createdAt: "09/12/2021",
-      updatedAt: "09/12/2021",
-    },
-    {
-      id: "1Ot-lfQvZtOFrBv78hTmLT8SrMR3qflEs",
-      title: "Your Name",
-      description: "this is description",
-      userId: "1",
-      commentable: true,
-      thumbNailUrl: "https://images2.alphacoders.com/742/thumbbig-742320.webp",
-      size: 1000,
-      createdAt: "09/12/2021",
-      updatedAt: "09/12/2021",
-    },
-    {
-      id: "1D2Jya5U-kcuRjJSPJSfi4ZMJm1AQSyAx",
-      title: "Fairy Tail",
-      description: "this is description",
-      userId: "1",
-      commentable: true,
-      thumbNailUrl: "https://images6.alphacoders.com/311/thumbbig-311015.webp",
-      size: 1000,
-      createdAt: "09/12/2021",
-      updatedAt: "09/12/2021",
-    },
-  ];
+    notifyOnNetworkStatusChange: true
+  })
+
+  const loadingMore = networkStatus === NetworkStatus.fetchMore
+
+  const loadMore = () => {
+    fetchMore({variables: {cursor: data?.videos?.cursor}})
+  }
 
   return (
     <div className={styles.home}>
       <SlickNav />
       <div className={styles.layout}>
-        {data.map((video, index) => (
+        {data?.videos?.paginatedVideos.map((video, index) => (
           <Link to={`/watch/${video.id}`} key={index}>
             <div className={styles["layout-item"]}>
               <img
-                src={video.thumbNailUrl}
+                src={video.thumbnailUrl || 'https://images6.alphacoders.com/311/thumbbig-311015.webp'}
                 alt="img"
                 className={styles["layout-img"]}
               />
@@ -143,6 +47,12 @@ const Home = () => {
           </Link>
         ))}
       </div>
+      {!data?.videos && !loading && <h1 style={{marginTop: '20px'}}>Không có dữ liệu</h1>}
+      
+      {data?.videos?.hasMore && (
+        <button onClick={loadMore}>Load More</button>
+      )}
+      
     </div>
   );
 };

0aca2c002a25c3bd3d8491ee0ff24965db062cce
Đức Thắng Trần Hậu <ducthang9122001@gmail.com>
2021-12-29 00:17:34 +0700

    add contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,28 +1,59 @@
 import React, { useState } from "react";
-import styles from "./Contact.module.scss"; 
+import styles from "./Contact.module.scss";
 const Contact = () => {
   const [Name, setName] = useState("");
-  const [Email, setEmail] = useState(""); 
-  const [Message, setMessage] = useState("")  
-  const submitHandler = (e: React.SyntheticEvent) =>{ 
+  const [Email, setEmail] = useState("");
+  const [Message, setMessage] = useState("");
+
+  const nameChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    setName(e.currentTarget.value);
+  };
+
+  const emailChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const messageChangeHandler = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    setMessage(e.currentTarget.value);
+  };
+
+  const submitHandler = (e: React.SyntheticEvent) => {
     e.preventDefault();
 
-    // return default state 
+    // return default state
     setName("");
-    setEmail(""); 
-    setMessage(""); 
-  }
+    setEmail("");
+    setMessage("");
+  };
 
   return (
-    <div className={styles["contact-form"]} >
+    <div className={styles["contact-form"]}>
       <form onSubmit={submitHandler}>
         <h2>CONTACT US</h2>
-    
-        <input type="text" placeholder="Name" value={Name} required/>
-  
-        <input type="email" placeholder="Email" value={Email} required/>
 
-        <input type="text" placeholder="Message" value={Message} required/>
+        <input
+          type="text"
+          placeholder="Name"
+          value={Name}
+          onChange={nameChangeHandler}
+          required
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={Email}
+          onChange={emailChangeHandler}
+          required
+        />
+
+        <textarea
+          placeholder="Message"
+          value={Message}
+          onChange={messageChangeHandler}
+          rows={5}
+          maxLength={200}
+        ></textarea>
 
         <button type="submit">SEND</button>
       </form>

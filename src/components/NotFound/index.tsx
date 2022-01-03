b2d8a2172a0f45b8e6e1faa704e59b1398c84c42
Dark-Star3110 <dangvan31102001@gmail.com>
2022-01-03 11:37:59 +0700

    fix not found page

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,10 +1,41 @@
 import styles from "./NotFound.module.scss";
-import notFound_img from "../../assets/img/not_found.jpg";
+import { Link } from "react-router-dom";
+
+import logo404 from "../../assets/img/404_logo.svg";
+import astronaut from "../../assets/img/astronaut.png";
+import mars from "../../assets/img/mars.svg";
+import spaceship from "../../assets/img/spaceship.svg";
+import star from "../../assets/img/star.svg";
 
 const NotFound = () => {
   return (
-    <div className={styles.notfound}>
-      <img src={notFound_img} alt="" />
+    <div
+      className={styles.notfound}
+      style={{
+        backgroundImage: `url(${star}),linear-gradient(to bottom, #05007a, #4d007d)`,
+      }}
+    >
+      <div
+        className={styles["mars"]}
+        style={{
+          background: `url(${mars}) no-repeat bottom center`,
+          backgroundSize: "cover",
+        }}
+      ></div>
+      <img src={logo404} className={styles["logo-404"]} alt="img" />
+
+      <p className={styles["title"]}>Oh no!!</p>
+      <p className={styles["subtitle"]}>
+        Bạn có thể đã viết sai URL <br /> hoặc yêu cầu được gửi đến một trang
+        không tồn tại.
+      </p>
+      <div className={styles["back-home"]}>
+        <Link className={styles["btn-back"]} to="/">
+          Quay lại Trang Chủ
+        </Link>
+      </div>
+      <img src={astronaut} className={styles["astronaut"]} alt="img" />
+      <img src={spaceship} className={styles["spaceship"]} alt="img" />
     </div>
   );
 };
